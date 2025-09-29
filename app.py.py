612e502880221d6007a5c33410f5aff4f571e63f
Muhammad Taha <mtahashahid230@gmail.com>
2025-09-29 03:59:45 -0700

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -210,9 +210,9 @@ def main():
                 key="tickers_input"
             )
             user_query = st.text_input("Your Query:", key="user_query")
-            st.write("Selected tickers:", selected)
-            return tickers_input, user_query, 
-    
+            # st.write("Selected tickers:", selected)
+            return tickers_input, user_query
+      st.write("tickers_input",tickers_input)
     chat_container = st.container()
     if "messages" not in st.session_state:
         st.session_state.messages = []
